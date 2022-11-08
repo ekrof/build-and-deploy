80aad84db48bcc4557f3f10ac1005c6d641e774e
Jonas Ekerhovd <jonas.ekerhovd@tietoevry.com>
2022-11-08 19:23:45 +0100

    added xmas

--- code/src/clock.js
+++ code/src/clock.js
@@ -17,3 +17,15 @@ export const getSecondsLeftOfYear = (now) => {
     const nowMillis = now.getTime();
     return Math.floor((startOfNextYearMillis - nowMillis) / 1000);
 }
+
+export const getDaysUntilXmas = (now) => {
+    var today=new Date(now);
+    var xmas=new Date(today.getFullYear(), 11, 24);
+    if (today.getMonth()==11 && today.getDate()>24) 
+    {
+    xmas.setFullYear(xmas.getFullYear()+1); 
+    }
+    var one_day=1000*60*60*24;
+    xmas = Math.ceil((xmas.getTime()-today.getTime())/(one_day))  
+    return xmas;
+}
--- code/src/main.js
+++ code/src/main.js
@@ -1,12 +1,14 @@
-import {  getSecondsLeftOfYear, getTimeString } from "./clock";
+import {  getSecondsLeftOfYear, getTimeString, getDaysUntilXmas} from "./clock";
 
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
+const timeChristmas = document.getElementById("xmas");
 
 const render = () => {
   const now = new Date();
   timeLeft.innerText = getSecondsLeftOfYear(now);
   timeContainer.innerText = getTimeString(now);
+  timeChristmas.innerText = getDaysUntilXmas(now);
 }
 
 render();
